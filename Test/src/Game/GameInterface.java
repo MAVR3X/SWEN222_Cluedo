f2604c7133df8a1cd4ecc2dfcffb145fdb8086ad
NewZulund <NewZulund@users.noreply.github.com>
2014-08-16 14:28:02 +1200

    Removed green squre

--- Test/src/Game/GameInterface.java
+++ Test/src/Game/GameInterface.java
@@ -102,7 +102,7 @@ public class GameInterface extends JFrame implements ActionListener,
 		//PlayerPanel gui = new PlayerPanel();
 		PlayerPanel gui = new PlayerPanel(board);
 		gui.setBounds(0, CANVAS_HEIGHT, CANVAS_WIDTH, FRAME_HEIGHT - CANVAS_HEIGHT);
-		gui.setBackground(Color.PINK);
+		gui.setBackground(Color.BLACK);
 		JButton button = new JButton("Roll Dice");
 		button.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
